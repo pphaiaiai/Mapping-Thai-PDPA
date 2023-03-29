99a9556114e7e2f501e0cad449ab1ee475f344d1
Tarathep Siripis <tarathep.siripis@mail.kmutt.ac.th>
2023-03-29 16:06:06 +0700

    Add slug

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const fs = require('fs');
 async function main() {
     const dataSource = []
     for (let i = 1; i <= 96; i++) {
-        fs.readFile(`./content/article-${i}.md`, 'utf8', (err, data) => {
+        fs.readFile(`./content/article-${i}.md`, 'utf8', async (err, data) => {
             if (err) throw err;
             const dataSplit = data.split('\n')
-            console.log('data', dataSplit);
-            console.log('data split length =-=-=-=-=-=-=-=->>', dataSplit.length);
+            // console.log('data', dataSplit);
+            // console.log('data split length =-=-=-=-=-=-=-=->>', dataSplit.length);
             const lawData = {}
             lawData.title = dataSplit[1]
-            lawData.title = lawData.title.substring(7, lawData.title.length - 1)
+            lawData.title = await lawData.title.substring(7, lawData.title.length - 1)
+            // console.log(lawData.title +"=-=-=-=-=-=-=-=-==-=-=-=-=-=-=",i);
             const content = []
             for (let j = 6; j < dataSplit.length; j++) {
                 const datasp = dataSplit[j]
@@ -24,14 +25,15 @@ async function main() {
             if (newData.includes('\r') && newData.indexOf('\r') === newData.length - 2) {
                 newData = newData.substring(0, newData.length - 2)
             }
-            newData = newData.replaceAll(/\r/g, '\n')
-            newData = newData.replaceAll(/&emsp;/g, '\t')
+            newData = await newData.replaceAll(/\r/g, '\n')
+            newData = await newData.replaceAll(/&emsp;/g, '\t')
 
             lawData.content = newData
+            lawData.slug = i
             dataSource.push(lawData)
             console.log('dataSource', dataSource);
             fs.writeFile('pdpa.json', JSON.stringify(dataSource), (err) => {
-                console.log('err', err);
+                // console.log('err', err);
             });
         });
     }
